dc9249a214f18a0768f311e48c38042ce5f94e6a
saberlee66 <saberazizi66@yahoo.com>
2020-08-28 19:56:42 +0430

    add microservice handle command and create event

---
+++ saber-microservice-command-and-event/src/main/java/com/saber/rmicroservice/commandand/event/SaberMicroserviceCommandAndEventApplication.java
@@ -0,0 +1,13 @@
+package com.saber.rmicroservice.commandand.event;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaberMicroserviceCommandAndEventApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaberMicroserviceCommandAndEventApplication.class, args);
+	}
+
+}
