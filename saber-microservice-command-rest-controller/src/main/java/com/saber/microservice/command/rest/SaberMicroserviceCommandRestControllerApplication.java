1b1580116fd4a4d3ad49992703dfa840b458c928
saberlee66 <saberazizi66@yahoo.com>
2020-08-28 19:03:32 +0430

    add microservice command rest controller

---
+++ saber-microservice-command-rest-controller/src/main/java/com/saber/microservice/command/rest/SaberMicroserviceCommandRestControllerApplication.java
@@ -0,0 +1,13 @@
+package com.saber.microservice.command.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaberMicroserviceCommandRestControllerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaberMicroserviceCommandRestControllerApplication.class, args);
+	}
+
+}
---
+++ saber-microservice-command-rest-controller/src/test/java/com/saber/microservice/command/rest/SaberMicroserviceCommandRestControllerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saber.microservice.command.rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaberMicroserviceCommandRestControllerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
