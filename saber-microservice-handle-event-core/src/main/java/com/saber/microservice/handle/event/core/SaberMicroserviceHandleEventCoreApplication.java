283ed5ae4d1d619a4b21f0fc451bc050a9255fb9
saberlee66 <saberazizi66@yahoo.com>
2020-08-29 15:50:31 +0430

    add microservice event handle core

---
+++ saber-microservice-handle-event-core/src/main/java/com/saber/microservice/handle/event/core/SaberMicroserviceHandleEventCoreApplication.java
@@ -0,0 +1,13 @@
+package com.saber.microservice.handle.event.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaberMicroserviceHandleEventCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaberMicroserviceHandleEventCoreApplication.class, args);
+	}
+
+}
---
+++ saber-microservice-handle-event-core/src/test/java/com/saber/microservice/handle/event/core/SaberMicroserviceHandleEventCoreApplicationTests.java
@@ -0,0 +1,16 @@
+package com.saber.microservice.handle.event.core;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SaberMicroserviceHandleEventCoreApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
